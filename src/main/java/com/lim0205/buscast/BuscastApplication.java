c40fa903de7d2ac84074c065dffb0101b6e6a636
unknown <wlim3836@gmail.com>
2026-06-25 14:16:39 +0900

    Initialize Spring Boot project

---
+++ src/main/java/com/lim0205/buscast/BuscastApplication.java
@@ -0,0 +1,13 @@
+package com.lim0205.buscast;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BuscastApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BuscastApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lim0205/buscast/BuscastApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lim0205.buscast;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BuscastApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
